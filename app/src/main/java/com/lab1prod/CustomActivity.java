55b3bdceb9f57798902861ba7cea74e3aca08ac3
gvozd <Sachok1394@mail.ru>
2016-10-17 10:25:41 +0300

    add resorces

---
+++ app/src/main/java/com/lab1prod/CustomActivity.java
@@ -0,0 +1,13 @@
+package com.lab1prod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom);
+    }
+}
