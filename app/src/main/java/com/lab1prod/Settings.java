2cd795017080effcd6c520cae313459128398e80
gvozd <Sachok1394@mail.ru>
2016-11-15 23:45:09 +0300

    fix many attempts screen rotation

--- app/src/main/java/com/lab1prod/Settings.java
+++ app/src/main/java/com/lab1prod/Settings.java
@@ -11,6 +11,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -24,7 +25,8 @@ public class Settings extends AppCompatActivity implements View.OnClickListener 
     private static final String SETTINGS_KEY = "Settings Activity:";
     public static final String APP_PREFERENCES = "mysettings";
     private TextView DateTextView;
-    private TextView SoundValue;
+    private TextView SoundValue;//на Activity
+    private EditText DialogSound;
     private Button ColorButton;
     private ActionBar actionBar;
     private int GlobalColor = 2130837582;//последний выбранный цвет
@@ -37,7 +39,22 @@ public class Settings extends AppCompatActivity implements View.OnClickListener 
     private SharedPreferences mSettings;
     private SharedPreferences.Editor editor;
     private String defSound = "la la la";
-    private boolean isCurrDate;
+    private int mn;
+    private String textSong = "";
+    private boolean dialogState = false;
+    private boolean DateDialogState = false;
+    private boolean soundDialogState = false;
+
+    private int day;
+    private int mounth;
+    private int year;
+    {
+        final Calendar c = Calendar.getInstance();
+        year = c.get(Calendar.YEAR);
+        mounth = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+
+    }
 
     //цвета int
     private static final int rED_Color = 25500;
@@ -45,6 +62,7 @@ public class Settings extends AppCompatActivity implements View.OnClickListener 
 
     private static final int SEA_COLOr = 4225536;//0255169
     private static final int PUrPLE = 25560239;
+    private static final int BLUE = 210255;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,40 +79,18 @@ public class Settings extends AppCompatActivity implements View.OnClickListener 
         NotificationSwitch.setChecked(true);
         AdvSwitch = (Switch) findViewById(R.id.buy_switch);
         AdvSwitch.setChecked(false);
-        final Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH)+1;
-        int day = c.get(Calendar.DAY_OF_MONTH);
-        CurrentDate = day + "." + month + "." + year;
-        DateTextView.setText(CurrentDate);
         mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        mn = mounth+1;
+        CurrentDate = day + "." + mn + "." + year;
+        DateTextView.setText(CurrentDate);
+        if (DialogSound!=null){
+            textSong = DialogSound.getText().toString();
+        }
     }
 
     public void onSetText(View view){
-        LayoutInflater layoutInflater = LayoutInflater.from(Settings.this);
-        View promptView = layoutInflater.inflate(R.layout.date_dialog, null);
-        DatePickerAndroid = (android.widget.DatePicker) promptView.findViewById(R.id.datePicker);
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Settings.this);
-        alertDialogBuilder.setView(promptView);
-        alertDialogBuilder.setCancelable(false)
-                .setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        int day = DatePickerAndroid.getDayOfMonth();
-                        int month = DatePickerAndroid.getMonth() + 1;
-                        int year = DatePickerAndroid.getYear();
-                        StoredDate = day + "." + month + "." + year;
-                        DateTextView.setText(StoredDate);
-                    }
-                })
-                .setNegativeButton(R.string.btn_camcel,
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-        AlertDialog alert = alertDialogBuilder.create();
-        alert.show();
-
+        DateDialogState = true;
+        onSetDateDialog(day,mounth,year);
         /*dateDialog = new com.lab1prod.DatePicker(DateTextView);
         dateDialog.show(getSupportFragmentManager(), "datePicker");
         dateDialog.setRetainInstance(true);*/
@@ -102,59 +98,14 @@ public class Settings extends AppCompatActivity implements View.OnClickListener 
 
     //sound dialog
     public void OnInputSound(View view){
-        LayoutInflater layoutInflater = LayoutInflater.from(Settings.this);
-        View promptView = layoutInflater.inflate(R.layout.text_input_dialog, null);
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Settings.this);
-        alertDialogBuilder.setView(promptView);
-
-        final EditText editText = (EditText) promptView.findViewById(R.id.edittext);
-        alertDialogBuilder.setCancelable(false)
-                .setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        SoundValue.setText(editText.getText());
-                    }
-                })
-                .setNegativeButton(R.string.btn_camcel,
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-        AlertDialog alert = alertDialogBuilder.create();
-        alert.show();
+        soundDialogState = true;
+        onShowSoundDialog(SoundValue.getText().toString());
     }
 
     //colo dialog
     public void onSetColor(View view){
-        LayoutInflater layoutInflater = LayoutInflater.from(Settings.this);
-        View promptView = layoutInflater.inflate(R.layout.color_dialog, null);
-        final  Button yButton = (Button) promptView.findViewById(R.id.yellow_color);
-        final Button rButton = (Button) promptView.findViewById(R.id.red_color);
-        final Button seaButton = (Button) promptView.findViewById(R.id.sea_color);
-        final Button whiteutton = (Button) promptView.findViewById(R.id.white_btn);
-        final Button purpleutton = (Button) promptView.findViewById(R.id.purple_btn);
-        whiteutton.setOnClickListener(this);
-        seaButton.setOnClickListener(this);
-        yButton.setOnClickListener(this);
-        rButton.setOnClickListener(this);
-        purpleutton.setOnClickListener(this);
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Settings.this);
-        alertDialogBuilder.setTitle(R.string.color_dialog);
-        alertDialogBuilder.setView(promptView);
-        alertDialogBuilder.setCancelable(false)
-                .setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-
-                    }
-                })
-                .setNegativeButton(R.string.btn_camcel,
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-        AlertDialog alert = alertDialogBuilder.create();
-        alert.show();
+        dialogState = true;
+        OnViewColorDialog();
     }
 
     @Override
@@ -210,6 +161,7 @@ public class Settings extends AppCompatActivity implements View.OnClickListener 
         /*editor = mSettings.edit();
         editor.clear();
         editor.commit();*/
+
         //sound
         if (SoundValue.getText().toString()==null){
             outState.putString("StoredSound",mSettings.getString("StoredSound", ""));
@@ -221,6 +173,7 @@ public class Settings extends AppCompatActivity implements View.OnClickListener 
             editor.putString("StoredSound", SoundValue.getText().toString());
             editor.apply();
         }
+        outState.putString("textSong",textSong);
 
         //color
         if (ColorButton.getBackground()==null){
@@ -247,15 +200,33 @@ public class Settings extends AppCompatActivity implements View.OnClickListener 
         }
         //outState.putString("Date",StoredDate);
         Log.d(SETTINGS_KEY,"Data add to bundle");
+        //диалоги флаги
+        outState.putBoolean("colorDialog",dialogState);
+        outState.putBoolean("dateDialog",DateDialogState);
+        outState.putBoolean("soundDialog",soundDialogState);
+
+        if (DatePickerAndroid!=null){//если мы не открывали диалог - dp будет null
+            outState.putInt("day",DatePickerAndroid.getDayOfMonth());
+            outState.putInt("month",DatePickerAndroid.getMonth());
+            outState.putInt("year",DatePickerAndroid.getYear());
+        }
+
         super.onSaveInstanceState(outState);
     }
 
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
+
         int restoreColor = savedInstanceState.getInt("Color");
         String restoreDate = savedInstanceState.getString("Date");
         String restoreSound = savedInstanceState.getString("StoredSound");
+
+        textSong = savedInstanceState.getString("textSong");
+        soundDialogState = savedInstanceState.getBoolean("soundDialog");
+        if (soundDialogState){
+            onShowSoundDialog(textSong);
+        }
         SoundValue.setText(restoreSound);
         switch (restoreColor){
             case rED_Color:{
@@ -274,22 +245,119 @@ public class Settings extends AppCompatActivity implements View.OnClickListener 
                 ColorButton.setBackgroundResource(R.color.sea_color);
                 break;
             }
-        }
-        DateTextView.setText(restoreDate);
-        /*if(restoreDate==null){
-            if(mSettings.contains("Data")){
-                ColorButton.setBackgroundResource(restoreColor);
-                String val = mSettings.getString("Data", "");
-                DateTextView.setText(val);
+            case BLUE:{
+                ColorButton.setBackgroundResource(R.color.blue);
+                break;
             }
         }
-        else {
-            editor = mSettings.edit();
-            DateTextView.setText(restoreDate);
-            editor.putString("Data", restoreDate);
-            editor.apply();
-        }*/
+        DateTextView.setText(restoreDate);
         Log.d(SETTINGS_KEY,"Reset sucs");
+        dialogState = savedInstanceState.getBoolean("colorDialog");
+        if (dialogState)
+        {
+            OnViewColorDialog();
+        }
+        DateDialogState = savedInstanceState.getBoolean("dateDialog");
+        int savedDay = savedInstanceState.getInt("day");
+        int savedMonth = savedInstanceState.getInt("month");
+        int savedYear = savedInstanceState.getInt("year");
+        if (DateDialogState){
+            onSetDateDialog(savedDay,savedMonth,savedYear);
+        }
         super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    private void OnViewColorDialog(){
+        LayoutInflater layoutInflater = LayoutInflater.from(Settings.this);
+        View promptView = layoutInflater.inflate(R.layout.color_dialog, null);
+        final  Button yButton = (Button) promptView.findViewById(R.id.yellow_color);
+        final Button rButton = (Button) promptView.findViewById(R.id.red_color);
+        final Button seaButton = (Button) promptView.findViewById(R.id.sea_color);
+        final Button whiteutton = (Button) promptView.findViewById(R.id.white_btn);
+        final Button purpleutton = (Button) promptView.findViewById(R.id.purple_btn);
+        whiteutton.setOnClickListener(this);
+        seaButton.setOnClickListener(this);
+        yButton.setOnClickListener(this);
+        rButton.setOnClickListener(this);
+        purpleutton.setOnClickListener(this);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Settings.this);
+        alertDialogBuilder.setTitle(R.string.color_dialog);
+        alertDialogBuilder.setView(promptView);
+        alertDialogBuilder.setCancelable(false)
+                .setNegativeButton(R.string.btn_ok,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialogState = false;
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+    public void onSetDateDialog(final int d,final int m,final int y){
+        LayoutInflater layoutInflater = LayoutInflater.from(Settings.this);
+        View promptView = layoutInflater.inflate(R.layout.date_dialog, null);
+        DatePickerAndroid = (android.widget.DatePicker) promptView.findViewById(R.id.datePicker);
+        DatePickerAndroid.init(y,m,d,null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Settings.this);
+        alertDialogBuilder.setView(promptView);
+        alertDialogBuilder.setCancelable(false)
+               /* .setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        int day = DatePickerAndroid.getDayOfMonth();
+                        int month = DatePickerAndroid.getMonth() + 1;
+                        int year = DatePickerAndroid.getYear();
+                        StoredDate = day + "." + month + "." + year;
+                        DateTextView.setText(StoredDate);
+                        DateDialogState = false;
+                    }
+                })*/
+                .setNegativeButton(R.string.btn_ok,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                int day = DatePickerAndroid.getDayOfMonth();
+                                int month = DatePickerAndroid.getMonth() + 1;
+                                int year = DatePickerAndroid.getYear();
+                                StoredDate = day + "." + month + "." + year;
+                                DateTextView.setText(StoredDate);
+                                DateDialogState = false;
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+    public void onShowSoundDialog(final String song){
+        LayoutInflater layoutInflater = LayoutInflater.from(Settings.this);
+        View promptView = layoutInflater.inflate(R.layout.text_input_dialog, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Settings.this);
+        alertDialogBuilder.setView(promptView);
+
+        DialogSound  = (EditText) promptView.findViewById(R.id.edittext);
+        DialogSound.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(i==KeyEvent.KEYCODE_ENTER){
+                    textSong =DialogSound.getText().toString();
+                    return true;
+                }
+                return false;
+            }
+        });
+        DialogSound.setText(song);
+        alertDialogBuilder.setCancelable(false)
+                .setNegativeButton(R.string.btn_ok,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                SoundValue.setText(DialogSound.getText());//на аквивити
+                                textSong = DialogSound.getText().toString();
+                                soundDialogState = false;
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
     }
 }
